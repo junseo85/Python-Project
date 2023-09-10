cf408d3e9873049bb143331873b75fc3316f3326
Jun Seo <112793862+junseo85@users.noreply.github.com>
2023-09-10 01:10:00 -0500

    Leap_Year.py

--- Leap_Year.py
+++ Leap_Year.py
@@ -1,4 +1,5 @@
-
+#program that works out whether if a given year is a leap year.
+# it is a leap year if a particular year is evenly divisible by 4 EXCEPT every year is evenly divisible by 100 UNLESS is also evenly divisible by 400
 year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
